903b72e0f1a64efe9fd425a03f00926ef20cb145
dwinney <daniel.winney@gmail.com>
2025-05-13 07:29:28 -0700

    fixing botched merge

--- physics/isobars/pi1.hpp
+++ physics/isobars/pi1.hpp
@@ -19,17 +19,13 @@
 #include "kinematics.hpp"
 #include "settings.hpp"
 #include "GKPY.hpp"
-<<<<<<< HEAD
 #include "phase_shift.hpp"
-=======
->>>>>>> main
 
 namespace iterateKT
 { 
     // Ids for all our isobars, we only have one though
     enum class id : unsigned int { P_wave };
 
-<<<<<<< HEAD
     inline settings default_settings()
     {
         settings sets;
@@ -49,9 +45,6 @@ namespace iterateKT
         return sets;
     };
 
-=======
-    
->>>>>>> main
     // The P-wave is the dominant isobar
     // In terms of individual isobars this is the only one we need
     class P_wave : public raw_isobar
@@ -60,45 +53,16 @@ namespace iterateKT
         public: 
         
         // Constructor 
-<<<<<<< HEAD
         P_wave(isobar_args args) : raw_isobar(args){};
 
         inline uint    angular_momentum()   { return 1; };
         inline double  phase_shift(double s){ return GKPY::phase_shift(1,1, s); };
-=======
-        P_wave(isobar_args args) : raw_isobar(args) {};
-
-        inline uint    singularity_power()  { return 2; };
-        inline double  phase_shift(double s){ return GKPY::phase_shift(1, 1, s); };
->>>>>>> main
         inline complex ksf_kernel(id iso_id, complex s, complex t)
         { 
             if (iso_id != id::P_wave) return 0.;
             complex  k  = _kinematics->kacser(s), kz = _kinematics->kz(s,t);
             return -3/2*(k*k - kz*kz); // We've multiplied by k^2 which is why singularity_power() = 2
         };
-<<<<<<< HEAD
-=======
-
-        inline static const settings default_settings()
-        {
-            settings sets;
-            // sets._exclusion_points        = 10;
-            sets._infinitesimal           = 1E-7;
-            sets._intermediate_energy     = 4;
-            sets._cutoff                  = 20;
-            sets._interpolation_offset    = 0.1;
-            sets._interpolation_points    = {400, 10, 200};
-            double xi_sth = 1E-3,  eps_sth = 1E-3;
-            double xi_pth = 1E-3,  eps_pth = 1E-3;
-            double xi_rth = 1E-3,  eps_rth = 1E-3;
-
-            sets._exclusion_offsets   = {1E-1, 1E-1};
-            sets._matching_intervals  = {xi_sth,  xi_pth,  xi_rth };
-            sets._expansion_offsets   = {eps_sth, eps_pth, eps_rth};
-            return sets;
-        };
->>>>>>> main
     };
 }; // namespace iterateKT 
 
