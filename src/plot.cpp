00445a789edac673e6d1995a1687c9d60c984b9d
dwinney <daniel.winney@gmail.com>
2025-08-12 11:00:42 +0200

    2digits

--- src/plot.cpp
+++ src/plot.cpp
@@ -88,7 +88,7 @@ namespace iterateKT
 
         mg->GetXaxis()->CenterTitle(true);
         mg->GetYaxis()->CenterTitle(true);
-        mg->GetYaxis()->SetMaxDigits(2);
+        mg->GetYaxis()->SetMaxDigits(3);
         _canvas->Modified();
 
         double ylow, yhigh, xlow, xhigh;
