56fdcb059358da1609e62d64fcc64628ae163547
dwinney <daniel.winney@gmail.com>
2025-10-04 20:38:15 +0200

    added form factor into deck calculation

--- physics/amplitudes/pi1.hpp
+++ physics/amplitudes/pi1.hpp
@@ -25,17 +25,17 @@ namespace iterateKT
     inline settings default_settings()
     {
         settings sets;
-        sets._exclusion_points        = 6;
-        sets._infinitesimal           = 1E-7;
+        sets._exclusion_points        = 10;
+        sets._infinitesimal           = 1E-8;
         sets._intermediate_energy     = 4;
         sets._cutoff                  = 20;
         sets._interpolation_offset    = 0.1;
         sets._interpolation_points    = {200, 10, 100};
         double xi_sth = 1E-3,  eps_sth = 1E-3;
-        double xi_pth = 1E-3,  eps_pth = 1E-2;
+        double xi_pth = 3E-3,  eps_pth = 4E-3;
         double xi_rth = 3E-1,  eps_rth = 3E-1;
 
-        sets._exclusion_offsets   = {2E-1, 2E-1};
+        sets._exclusion_offsets   = {3E-2, 3E-2};
         sets._matching_intervals  = {xi_sth,  xi_pth,  xi_rth };
         sets._expansion_offsets   = {eps_sth, eps_pth, eps_rth};
         return sets;
@@ -54,8 +54,25 @@ namespace iterateKT
 
         static constexpr double _mu  = 0.13957000;
         static constexpr double _mu2 = _mu*_mu;
-        static constexpr double _eps = 1E-5;
 
+        static inline complex tau(complex t, complex M2, complex s, double z)
+        {
+            complex mu2 = complex(_mu2);
+            complex p   = csqrt(kallen(M2, t, mu2))/2/csqrt(M2);
+            complex q   = csqrt(kallen(M2, s, mu2))/2/csqrt(M2);
+            complex rho = 2*q/csqrt(M2);
+
+            // careful if we cross above the three-body cut
+            // multiply by -1 to not change sign and stay on the same sheet
+            bool above_3bcut = (real(s) >= real(M2)+_mu2);
+            if  (above_3bcut){ q *= -1; rho *= -1; };
+            return  mu2 + s - (M2+mu2-t)*(M2+s-mu2)/2/M2 + 2*p*q*z;
+        };
+
+        // 3P1 projection of the vanilla OPE
+        // t  -> momentum transfer of (external) Pomeron
+        // M2 -> total 3pi invariant mass
+        // s  -> 2pi subsystem imvariant mass
         static inline complex deck(complex t, complex M2, complex s)
         {
             // Masses and momenta
@@ -66,15 +83,62 @@ namespace iterateKT
 
             // careful if we cross above the three-body cut
             // multiply by -1 to not change sign and stay on the same sheet
-            bool above_tbc = (real(s) >= real(M2)+_mu2);
-            if  (above_tbc){ q *= -1; rho *= -1; };
-
+            bool above_3bcut = (real(s) >= real(M2)+_mu2);
+            if  (above_3bcut){ q *= -1; rho *= -1; };
+            // Momentum tranfer at costheta = 0
+            complex t0  = tau(t, M2, s, 0); 
             // Angular argument
-            complex z   = (M2-t-s+(_mu2-t)*(_mu2-s)/M2)/(4*p*q);
+            complex z   = (mu2 - t0)/2/p/q;
+
+            // Continution depends on the ieps used for s
+            // and not that of z
+            bool above_thr  = real(s) >= norm(2*_mu2);
+            int  sgn        = (sign(imag(s)) <= 0) ? +1 : -1;
             // Legendre of 2nd kind
-            complex Q0  = (imag(s) < 0) ? log(-(z+1)/(z-1))/2+I*PI/2: (log(-(z+1)/(z-1))-I*PI)/2;
+            complex Q0;
+            if (above_thr) Q0 = log(-(z+1)/(z-1))/2+I*sgn*PI/2;
+            else           Q0 = log( (z+1)/(z-1))/2;
+
             // Final discontinuity
             return rho*q/p*((1-z*z)*Q0+z);
+        };
+
+        // 3P1 projection of the OPE with an additional monopole FF
+        // t  -> momentum transfer of (external) Pomeron
+        // M2 -> total 3pi invariant mass
+        // s  -> 2pi subsystem imvariant mass
+        // L2 -> cutoff squared
+        static inline complex deck_with_FF(complex t, complex M2, complex s, double L2)
+        {
+            // We need the vanilla deck with normal on-shell pion
+            complex delta  = deck(t, M2, s);
+
+            complex mu2 = complex(_mu2);
+            complex p   = csqrt(kallen(M2, t, mu2))/2/csqrt(M2);
+            complex q   = csqrt(kallen(M2, s, mu2))/2/csqrt(M2);
+            complex rho = 2*q/csqrt(M2);
+           
+            // careful if we cross above the three-body cut
+            // multiply by -1 to not change sign and stay on the same sheet
+            bool above_3bcut = (real(s) >= real(M2)+_mu2);
+            if  (above_3bcut){ q *= -1; rho *= -1; };
+
+            // Calculate everything again but with different exchange mass
+            complex t0  = tau(t, M2, s, 0); 
+            complex z   = (mu2 - t0)/(2*p*q);
+            complex zp  = (L2  - t0)/(2*p*q);
+
+            // Continution depends on the ieps used for s
+            // and not that of z
+            bool above_thr  = real(s) >= norm(2*_mu2);
+            int  sgn        = (sign(imag(s)) <= 0) ? +1 : -1;
+            complex Q0p;
+            if (above_thr) Q0p = log(-(zp+1)/(zp-1))/2+I*sgn*PI/2;
+            else           Q0p = log( (zp+1)/(zp-1))/2;
+
+            complex deltap = rho*q/p*((1-2*z*zp+zp*zp)*Q0p+(2*z-zp));
+
+            return delta - deltap;
         };
 
         // Assuming a pi- pi- pi+ decay and only P-waves
